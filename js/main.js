19c629a9bb1086a56d0a70f50bd682d43922ae7b
Login Mern Stack 1 <gregorick.liriano@gmail.com>
2021-10-22 10:54:00 -0400

    Subir archivos

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var logo = document.getElementById("logo");
+var paloma = document.getElementById("paloma");
+
+// Animaciones intro logo
+
+setTimeout(() => {
+  logo.classList.add("activo");
+}, 300);
+setTimeout(() => {
+  paloma.classList.add("activo");
+}, 800);
